9894b10cc7d90e60e5b05bec51cc29f43d7fa211
anatoliylokatyr <andyone4311@gmail.com>
2023-03-14 10:47:26 +0100

    Added Barba

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,44 @@
-import { greetUser } from '$utils/greet';
+import barba from '@barba/core';
+import { restartWebflow } from '@finsweet/ts-utils';
+import { gsap } from 'gsap';
 
-window.Webflow ||= [];
-window.Webflow.push(() => {
-  const name = 'John Doe';
-  greetUser(name);
+barba.init({
+  transitions: [
+    {
+      name: 'default',
+      async leave(data) {
+        console.log('leave');
+        console.log(data);
+
+        await gsap.to(data.current.container, { opacity: 0, duration: 0.5 });
+      },
+      async enter(data) {
+        console.log('enter');
+        console.log(data);
+
+        await gsap.to(data.next.container, { opacity: 1, duration: 0.5 });
+      },
+    },
+    {
+      name: 'contact-transition',
+      from: {
+        namespace: ['home'],
+      },
+      to: {
+        namespace: ['contact'],
+      },
+    },
+  ],
+  views: [
+    {
+      namespace: 'contact',
+      afterEnter() {
+        console.log('enter contact');
+      },
+    },
+  ],
+});
+
+barba.hooks.after(async () => {
+  await restartWebflow();
 });
